205bff8dac5bd2f3756f0c35bd59457160476780
David Monaghan <github@allstruck.com>
2015-06-27 23:46:33 -0700

    Remove unappealing duplication.

--- app/assets/javascripts/situation-choice.js
+++ app/assets/javascripts/situation-choice.js
@@ -1,16 +1,4 @@
-$( document ).on('page:load', function(){
-  $('#situation_ending').change(function() {
-      console.log('changed ending option')
-      if( this.checked) {
-          $("#choices").hide()
-      } else {
-          $("#choices").show();
-      }
-      return false;
-  });
-  $('#situation_ending').trigger('change');
-});
-$( document ).ready(function(){
+$( document ).on('page:load ready', function(){
   $('#situation_ending').change(function() {
       console.log('changed ending option')
       if( this.checked) {
