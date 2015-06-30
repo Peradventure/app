390b6367dfd963630eb8b2268a00e3556947f2c0
David Monaghan <github@allstruck.com>
2015-06-29 22:03:03 -0700

    Remove comments of failed attempt to custom validate.

--- app/models/situation.rb
+++ app/models/situation.rb
@@ -1,15 +1,6 @@
-# class ChoicesSetUnlessEnding < ActiveModel::Validator
-#   def validate(record)
-#     unless record.name.starts_with? 'X'
-#       record.errors[:name] << 'Need a name starting with X please!'
-#     end
-#   end
-# end
-
 class Situation < ActiveRecord::Base
   validates :title, presence: true
   validates :sit_rep, presence: true
-  # validates_with ChoicesSetUnlessEnding
 
   validates :choice_1, presence: true,
                     unless: Proc.new { |a| a.ending? }
