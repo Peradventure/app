4736f03e6925aed31f59f50834c5c0430fac1576
Randy Brookins <randy@tanga.com>
2015-07-03 16:32:47 -0700

    check if choices are 0

--- app/controllers/situations_controller.rb
+++ app/controllers/situations_controller.rb
@@ -11,10 +11,10 @@ class SituationsController < ApplicationController
   # GET /situations/1.json
   def show
     @last_situation = @situation.id
-    @choice_1 = @situation.choice_1 ? @situation.choice_1 : "new?oid=#{@situation.id}"
-    @choice_2 = @situation.choice_2 ? @situation.choice_2 : "new?oid=#{@situation.id}"
-    @choice_1_label = @situation.choice_1 ? @situation.choice_1_label : "Create '#{@situation.choice_1_label}'"
-    @choice_2_label = @situation.choice_2 ? @situation.choice_2_label : "Create '#{@situation.choice_2_label}'"
+    @choice_1 = (@situation.choice_1 && @situation.choice_1 != 0) ? @situation.choice_1 : "new?oid=#{@situation.id}"
+    @choice_2 = (@situation.choice_2 && @situation.choice_2 != 0) ? @situation.choice_2 : "new?oid=#{@situation.id}"
+    @choice_1_label = @situation.choice_1_label
+    @choice_2_label = @situation.choice_2_label
   end
 
   # GET /situations/new
