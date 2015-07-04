c8ccbbe0018a8ebb07f46cfa871ea5c1bdf75081
David Monaghan <github@allstruck.com>
2015-07-04 01:04:26 -0700

    Fix link to create new from home view.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,8 +7,8 @@ class HomeController < ApplicationController
   def index
     @situation = Situation.find(params[:id] ? params[:id] : 1)
     @last_situation = @situation.id
-    @choice_1 = @situation.choice_1 ? @situation.choice_1 : "new?oid=#{@situation.id}"
-    @choice_2 = @situation.choice_2 ? @situation.choice_2 : "new?oid=#{@situation.id}"
+    @choice_1 = (@situation.choice_1 && @situation.choice_1 != 0) ? @situation.choice_1 : "situations/new?oid=#{@situation.id}&obutton=1"
+    @choice_2 = (@situation.choice_2 && @situation.choice_2 != 0) ? @situation.choice_2 : "situations/new?oid=#{@situation.id}&obutton=2"
     @choice_1_label = @situation.choice_1 ? @situation.choice_1_label : "Create '#{@situation.choice_1_label}'"
     @choice_2_label = @situation.choice_2 ? @situation.choice_2_label : "Create '#{@situation.choice_2_label}'"
   end
