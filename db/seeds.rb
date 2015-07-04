33468008c285e2960ab2f4c3c920a1a9c1849873
Randy Brookins <randy@tanga.com>
2015-07-03 19:29:30 -0700

    Matrix theme and infinite loop on the seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Situation.create(id: '1', title: 'Death', sit_rep: 'You dieded.', choice_1: '2', choice_1_label: 'Go to Heaven', choice_2: '3', choice_2_label: 'Go to Hell')
-Situation.create(id: '2', title: 'You are in Heaven.', sit_rep: "I hope you like it because you can't leave.", ending: true)
-Situation.create(id: '3', title: 'You are in Hell.', sit_rep: "If you want to get to heaven fuck you.", choice_1: '4', choice_1_label: 'Go to Heaven', choice_2: '0', choice_2_label: 'Stay In Hell' )
-Situation.create(id: '4', title: 'You are in Heaven.', sit_rep: "Fuck You", ending: true)
+Situation.create(id: '1', title: 'Wake up Neo...', sit_rep: 'You wake up at your terminal and see someone typed the words "Wake up Neo..."', choice_1: '2', choice_1_label: 'Type Back', choice_2: '3', choice_2_label: 'Turn Off Computer')
+Situation.create(id: '2', title: 'Who is this?', sit_rep: "You type back in 'Who is this?' and the FBI breaks down your door and arrests you. Game over man.", ending: true)
+Situation.create(id: '3', title: 'Blue Pill', sit_rep: "You turn off the computer but you notice the power cord wasn't even plugged in. You realize you must still be asleep and this is all a dream.", choice_1: '1', choice_1_label: 'Wake Up', choice_2: '0', choice_2_label: 'Stay in the dream.' )
