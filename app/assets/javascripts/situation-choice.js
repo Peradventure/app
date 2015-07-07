88d1281af3e7473ab1b00d4dea3f917f202940bf
Randy Brookins <randy@tanga.com>
2015-07-06 22:10:06 -0700

    stupid-proof the new situation form

--- app/assets/javascripts/situation-choice.js
+++ app/assets/javascripts/situation-choice.js
@@ -10,3 +10,45 @@ $( document ).on('page:load ready', function(){
   });
   $('#situation_ending').trigger('change');
 });
+
+$(function() {
+  $('a#show_title_help').click(function(event){
+    event.preventDefault();
+    $('div#title_help').toggle();
+  });
+});
+
+$(function() {
+  $('a#show_sit_rep_help').click(function(event){
+    event.preventDefault();
+    $('div#sit_rep_help').toggle();
+  });
+});
+
+$(function() {
+  $('a#show_choice_1_label_help').click(function(event){
+    event.preventDefault();
+    $('div#choice_1_label_help').toggle();
+  });
+});
+
+$(function() {
+  $('a#show_choice_1_help').click(function(event){
+    event.preventDefault();
+    $('div#choice_1_help').toggle();
+  });
+});
+
+$(function() {
+  $('a#show_choice_2_label_help').click(function(event){
+    event.preventDefault();
+    $('div#choice_2_label_help').toggle();
+  });
+});
+
+$(function() {
+  $('a#show_choice_2_help').click(function(event){
+    event.preventDefault();
+    $('div#choice_2_help').toggle();
+  });
+});
