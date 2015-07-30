e338383abcd5875c1f3acf3720130166c932d6c1
Randy Brookins <randy@tanga.com>
2015-07-29 20:38:54 -0700

    redirect to the right place after signup from new situation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    session[:previous_url] || root_path
+    session[:user_return_to] || session[:previous_url] || root_path
   end
 
 end
