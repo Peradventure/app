1e843bd1a9f6ddbe3e76854beb46d3cf25d4aa45
Randy Brookins <randy@tanga.com>
2015-09-06 18:33:04 -0700

    move the game off the front page and make local seeds work

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get '/game' => 'game#show'
   get '/:id' => 'home#index'
+
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@
 Situation.create(id: '1', title: 'Wake up Neo...', sit_rep: 'You wake up at your terminal and see someone typed the words "Wake up Neo..."', choice_1: '2', choice_1_label: 'Type Back', choice_2: '3', choice_2_label: 'Turn Off Computer')
 Situation.create(id: '2', title: 'Who is this?', sit_rep: "You type back in 'Who is this?' and the FBI breaks down your door and arrests you. Game over man.", ending: true)
 Situation.create(id: '3', title: 'Blue Pill', sit_rep: "You turn off the computer but you notice the power cord wasn't even plugged in. You realize you must still be asleep and this is all a dream.", choice_1: '1', choice_1_label: 'Wake Up', choice_2: '0', choice_2_label: 'Stay in the dream.' )
+Situation.create(id: '26', title: 'Welcome', sit_rep: "This is the first page you see", choice_1: '1', choice_1_label: 'Wake Up', choice_2: '0', choice_2_label: 'Stay in the dream.' )
 
 User.create(id: '1', email: 'randy@tanga.com', password: 'password', admin: false)
 User.create(id: '2', email: 'david@tanga.com', password: 'password', admin: false)
